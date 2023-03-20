3387017da58f486beed318673d0aee322f391452
Medranomiler1 <medranomiler@gmail.com>
2023-03-20 14:37:58 -0500

    added layout page

---
+++ src/components/navbar/navbar.js
@@ -0,0 +1,27 @@
+import { Button, Menu, MenuList, MenuButton, MenuItem} from "@chakra-ui/react";
+import styles from "./styles.module.css";
+import Link from "next/link";
+
+const Navbar = () => {
+
+  return (
+    <div className={styles.navbar}>
+      <Menu>
+        <MenuButton as={Button} colorScheme="whiteAlpha" bg="gray.50">
+          <svg className={styles.icon} viewBox="0 0 100 80" width="40" height="40">
+            <rect width="100" height="20" rx="10"></rect>
+            <rect y="30" width="100" height="20" rx="10"></rect>
+            <rect y="60" width="100" height="20" rx="10"></rect>
+          </svg>
+        </MenuButton>
+        <MenuList bg="gray.50">
+          <MenuItem color="black"><Link href="/">Home</Link></MenuItem>
+          <MenuItem color="black"><Link href="/contact">Contact</Link></MenuItem>
+        </MenuList>
+      </Menu>
+      <h1 className={styles.title} fontSize={"xl"}>Darren Medrano</h1>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/_app.js
@@ -0,0 +1,14 @@
+import Layout from "../components/layout/layout"
+import { ChakraProvider } from "@chakra-ui/react";
+import "../styles/globals.css"
+
+export default function App({
+  Component, pageProps}) {
+  return (
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+  );
+}
