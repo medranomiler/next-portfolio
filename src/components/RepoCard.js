1fba7f73c671d9092c51b0dbd9b63182cbb22bdd
Medranomiler1 <medranomiler@gmail.com>
2023-03-28 15:07:06 -0500

    added flip effect to repo cards

--- src/components/RepoCard.js
+++ src/components/RepoCard.js
@@ -1,32 +1,53 @@
-import { Card, CardHeader, CardBody, CardFooter, Heading, Text, Box, Flex, Icon } from "@chakra-ui/react";
-import {FaReact, FaBootstrap, FaNode, FaLink} from "react-icons/fa"
-import { SiMongodb, SiMysql, SiExpress, SiVercel, SiTailwindcss, SiOpenai, SiGraphql, SiApollographql, SiChakraui, SiJavascript, SiNextdotjs, SiGithub} from "react-icons/si"
-import Link from "next/link";
-import { useEffect, useState } from "react"
+import { Card, CardHeader, CardBody, CardFooter, Heading, Text, Box, Flex, Icon, AbsoluteCenter, Center } from "@chakra-ui/react";
+import { FaReact, FaBootstrap, FaNode, FaGithub } from "react-icons/fa";
+import { SiMongodb, SiMysql, SiExpress, SiVercel, SiTailwindcss, SiOpenai, SiGraphql, SiApollographql, SiChakraui, SiJavascript, SiNextdotjs, SiGithub } from "react-icons/si";
+import { useEffect, useState } from "react";
+import Link from "next/link"
 
-const RepoCard = ({ repo }) =>{
+const RepoCard = ({ repo }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
 
+  const handleClick = () => {
+    setIsFlipped(!isFlipped);
+  };
 
   return (
     <Box m={{
       base: "4",
       md: "4"}}>
-    <Link href={repo.html_url}>
-      <Card bg="white" animation="glow 2s ease-in-out infinite" maxW="md" minW={{
-        base:"xs",
-        md:"md"}}  minH="sm">
-        <CardHeader>
-          <Heading color="black" textAlign="center" size="2xl" py={2}>
-            {repo.name}
-          </Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>{repo.description}</Text>
-        </CardBody>
-        <CardFooter
-        justify='center'
-        flexWrap='wrap'>
-{repo.topics.map((topic) => {
+      <div style={{
+        perspective: "1000px"
+      }}>
+        <Card bg="white" animation="glow 2s ease-in-out infinite" maxW="md" minW={{
+          base: "xs",
+          md: "md"}}  minH="sm"
+          style={{
+            transformStyle: "preserve-3d",
+            transition: "transform 0.5s",
+            transform: isFlipped ? "rotateY(180deg)" : "rotateY(0deg)"
+          }}
+          borderRadius={30}
+          onClick={handleClick}
+        >
+          <div style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backfaceVisibility: "hidden"
+          }}>
+            <CardHeader>
+              <Heading color="black" textAlign="center" size="2xl" py={2}>
+                {repo.name}
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Text>{repo.description}</Text>
+            </CardBody>
+            <CardFooter
+              justify='center'
+              flexWrap='wrap'
+            >
+              {repo.topics.map((topic) => {
              if (topic === "chakra-ui") {
               return (
                 <Box key={topic} mx={3}>
@@ -112,11 +133,31 @@ const RepoCard = ({ repo }) =>{
               );
             }
           })}
-        </CardFooter>
-      </Card>
-    </Link>
+            </CardFooter>
+          </div>
+          <div style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            backfaceVisibility: "hidden",
+            transform: "rotateY(180deg)"
+          }}>
+                        <CardHeader>
+              <Heading color="black" textAlign="center" size="2xl" py={2}>
+                View The Repository on GitHub
+              </Heading>
+            </CardHeader>
+            <CardBody>
+              <Box>   
+              <Center><Link href={repo.html_url}><Icon as={FaGithub} height={100} width={100} /></Link></Center>  
+              </Box>
+            </CardBody>
+          </div>
+        </Card>
+      </div>
     </Box>
   );
-}
+};
+
 
 export default RepoCard;
