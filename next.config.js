0af91695ddfe1f87d71065abda1a8fa9549bc8f9
Medranomiler1 <medranomiler@gmail.com>
2023-03-31 16:44:59 -0500

    updated next config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
+module.exports = {
+  env: {
+    MONGO_URI: "mongodb+srv://medranomiler:root1234@cluster0.kuljfho.mongodb.net/MyApp"
+  },
+};
 
-
-module.exports = { nextConfig }
+module.exports =  nextConfig 
