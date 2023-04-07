4f51d97d9d2edde40602143e556482b48ec92b23
Medranomiler1 <medranomiler@gmail.com>
2023-04-07 00:53:34 -0500

    edited text on collab cards for better fit of content and icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,8 +27,8 @@ const Portfolio = () => {
         <div className="h-1/3">
         <h2 className="lg:text-5xl md:text-4xl text-3xl font-bold dark:text-gray-400 p-4">Portfolio</h2>
         <div className="flex">
-          <h3 className={`cursor-pointer lg:text-4xl md:text-3xl text-2xl font-light mr-10 dark:text-gray-400 p-4 ${projects === "personal" && "text-blue-500 dark:text-blue-500"}`} onClick={() => projectHandler("personal")}>Personal</h3>
-          <h3 className={`cursor-pointer lg:text-4xl md:text-3xl text-2xl font-light  dark:text-gray-400 p-4 ${projects === "collabs" && "text-blue-500 dark:text-blue-500"}`} onClick={() => projectHandler("collabs")}>Collaborations</h3>
+          <h3 className={`cursor-pointer lg:text-4xl md:text-3xl text-2xl font-light mr-10 ${projects !== "personal" && "text-gray-400 dark:text-gray-400"} p-4 ${projects === "personal" && "text-blue-500 dark:text-blue-500"}`} onClick={() => projectHandler("personal")}>Personal</h3>
+          <h3 className={`cursor-pointer lg:text-4xl md:text-3xl text-2xl font-light  ${projects !== "collabs" && "text-gray-400 dark:text-gray-400"} p-4 ${projects === "collabs" && "text-blue-500 dark:text-blue-500"}`} onClick={() => projectHandler("collabs")}>Collaborations</h3>
         </div>
         </div>
         <div className="h-1/3">
