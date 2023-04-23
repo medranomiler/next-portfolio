b374abb128abff5d6c81c4ab642599b9ebf1f10c
Medranomiler1 <medranomiler@gmail.com>
2023-04-23 16:53:01 -0500

    added dark styles to login/create pages

--- src/pages/login.js
+++ src/pages/login.js
@@ -36,15 +36,15 @@ const Login = () => {
 
 
   return (
-    <div className="max-w-screen py-36 p-4 relative flex flex-col items-center lg:m-16 aboutMe">
+    <div className="max-w-screen h-screen p-4 py-36 relative flex flex-col items-center bg-slate-50 dark:bg-gray-950">
        {loggedIn? <button className="bg-gray-200 p-2 rounded" onClick={handleLogout}>Log Out</button> : 
        (<>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-        <h1 className="text-3xl">Login</h1>
+        <h1 className="text-4xl text-gray-900 dark:text-white">Login</h1>
         <form className="p-4 relative flex flex-col items-center max-w-[400px] w-full" onSubmit={handleFormSubmit}>
-       <input className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-2"  type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
-        <input className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-2" type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
-        <button type="submit" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800">
+       <input className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"  type="text" placeholder="username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+        <input className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+        <button type="submit" className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-violet-500 rounded-lg hover:bg-black dark:hover:bg-gray-800">
        Login
    </button>
         </form>
--- src/pages/repos/new.js
+++ src/pages/repos/new.js
@@ -8,10 +8,11 @@ import { AuthContext } from "../../components/AuthContext"
 const NewThought = () => {
     const [name, setName] = useState('');
     const [description, setDescription] = useState('');
-    const [topics, setTopics] = useState([]);
+    const [topics, setTopics] = useState("");
     const [html_url, setHtml_url] = useState('')
     const [image, setImage] = useState('')
     const [deployedUrl, setDeployedUrl] = useState('')
+    const [loading, setLoading] = useState(false)
     const [errorMessage, setErrorMessage] = useState('');
     const { loggedIn } = useContext(AuthContext);
 
@@ -26,53 +27,61 @@ const NewThought = () => {
         const { error } = await response.json()
         
         if(!error){
+            setLoading(false)
             Router.push('/')
         }
         else {
+          setLoading(false)
           setErrorMessage(error);
         }
       };
 
 
   return (
-    <div className="max-w-screen py-36 p-4 relative flex flex-col items-center lg:m-16 aboutMe">
+    <div className="max-w-screen h-screen py-36 p-4 relative flex flex-col items-center bg-slate-50 dark:bg-gray-950">
       {loggedIn? (<>
-    <h1 className="text-3xl">Create New Repo</h1>
+    <h1 className="text-4xl text-gray-900 dark:text-white">Create New Repo</h1>
         <form className="p-4 relative flex flex-col items-center md:w-1/3 lg:w-1/2 w-full"
         onSubmit={handleSubmit}>
             <input type="text" 
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-2" placeholder="name"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" placeholder="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
             />
             <input type="text" 
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-2" placeholder="description"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"  placeholder="description"
             value={description}
             onChange={(e) => setDescription(e.target.value)}
             />
             <input type="text" 
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-2" placeholder="topics"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500"  placeholder="topics"
             value={topics}
             onChange={(e) => setTopics(e.target.value)}
             />
             <input type="text" 
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-2" placeholder="repo url"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" placeholder="repo url"
             value={html_url}
             onChange={(e) => setHtml_url(e.target.value)}
             />
             <input type="text" 
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-2" placeholder="image url"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" placeholder="image url"
             value={image}
             onChange={(e) => setImage(e.target.value)}
             />
             <input type="text" 
-            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 mb-2" placeholder="deployed url"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-100 rounded-lg border border-gray-300 focus:ring-violet-500 focus:border-violet-500 mb-2 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-violet-500 dark:focus:border-violet-500" placeholder="deployed url"
             value={deployedUrl}
             onChange={(e) => setDeployedUrl(e.target.value)}
             />    
-               <button type="submit" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 hover:bg-blue-800">
+               {loading ? (<button disabled type="button" className="py-2.5 px-5 mt-4 sm:mt-6 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 inline-flex items-center">
+              <svg aria-hidden="true" role="status" className="inline w-4 h-4 mr-3 text-gray-200 animate-spin dark:text-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor" />
+                <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="#1C64F2" />
+              </svg>
+              Loading...
+            </button>) : <button type="submit" class="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-violet-500 rounded-lg hover:bg-black dark:hover:bg-gray-800">
        Publish Repo
-   </button>
+   </button>}
         </form>
         {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         </>) : (
@@ -86,3 +95,4 @@ const NewThought = () => {
 }
 
 export default NewThought
+
