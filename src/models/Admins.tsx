b792e9a1629f093017aa83d29d8b24ebbe4584c0
Medranomiler1 <medranomiler@gmail.com>
2023-04-28 15:00:30 -0500

    updated admin model for sign up

--- src/models/Admins.tsx
+++ src/models/Admins.tsx
@@ -3,6 +3,8 @@ import { Schema, model, models } from "mongoose";
 const AdminSchema = new Schema({
   username: { type: String, required: true },
   password: { type: String, required: true },
+  profilePhoto: { type: String, required: true },
+  bio: { type: String, required: true },
   repos: [{ type: Schema.Types.ObjectId, ref: "Repo" }]
 });
 
