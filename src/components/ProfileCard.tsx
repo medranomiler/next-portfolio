5fd23a4b04b5e903954665c569f75aaa7e7a4318
Medranomiler1 <medranomiler@gmail.com>
2023-06-19 16:19:09 -0500

    added new profile image

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,7 +1,9 @@
 import avatar from "../Assets/avatar.png"
 import Image from "next/image"
 import useFetchProfile from "../../hooks/useFetchProfile"
+import { useState } from "react"
 import { Lato } from "next/font/google"
+import myPhoto from "../assets/tracedPortrait2.svg"
 
 const lato = Lato({
   weight: ['400' , '700'],
@@ -11,13 +13,25 @@ const lato = Lato({
 
 const ProfileCard = () => {
   const [username, bio, profilePhoto] = useFetchProfile()
+  const [isMe, setIsMe ] = useState(false)
 
+  async function checkIsMe() {
+    await profilePhoto;
+    if (
+      profilePhoto ===
+      "https://avatars.githubusercontent.com/u/67513942?v=4"
+    ) {
+      setIsMe(true);
+    }
+  }
+
+  checkIsMe();
 
   return (
     <>
       <div className={`${lato.className} bg-transparent lg:p-10 py-8`}>
         <div className="flex flex-wrap items-center justify-center">
-          <img className="w-44 mb-4 lg:mb-0 rounded-full" src={profilePhoto} alt="profileImage" />
+        {isMe? <Image className="w-44 h-auto rounded-full mb-4 lg:mb-0 " src={myPhoto} alt="profileImage" /> : <img className="w-44 mb-4 lg:mb-0 rounded-full" src={profilePhoto} alt="profileImage" /> }
           <div className="lg:ml-20 md:ml-30 sm:ml-20  dark:text-gray-400">
             <p className=" lg:text-7xl md:text-5xl text-5xl font-bold">{username}</p>
             <p className="font-light">{bio}</p>
